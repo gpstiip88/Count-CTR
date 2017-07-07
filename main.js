1903dded6c93539e82586dd48d80a91bdcd1103d
Gabriele Pieretti <gabriele.pieretti@gmail.com>
2017-07-07 22:56:28 +0200

    aggiunta gestione calcolo da api aggiunto js per calcolo live

---
+++ main.js
@@ -0,0 +1,32 @@
+(function($){
+	function getTitlePoints(title, titleClass, setTips = false){
+		$.ajax({
+			type: "POST",
+			url: 'http://dev.stiip.it/api-ctr/calc-ctr.php',
+			data: {
+				title: title
+			},
+			success: function(response){
+				var points = response.points;
+				$('#'+titleClass+' .c100').removeClass().addClass('c100 p'+points);
+				$('#'+titleClass+' .points').html(points);
+
+				if(setTips){
+					$('#tips').html('');
+					$.each(response.tips, function(index, el){
+						$('#tips').append('<div class="cornice"><p><span id="'+index+'" class="text-font">Suggerimento: </span>'+el+'</p></div>')
+					});
+				}
+			}
+		});
+	}
+
+	$(document).on('change keyup', '#title', function(e){
+		getTitlePoints($(this).val(), 'original-title', true);
+	});
+
+	$(document).on('change keyup', '#subtitle', function(e){
+		getTitlePoints($(this).val(), 'alternative-title');
+	});
+
+})(jQuery);
