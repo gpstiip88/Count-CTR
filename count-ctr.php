82930184247f66e632b58e85a07fc934767d4eef
Andrea <andrea.tasselli@hotmail.it>
2017-07-24 15:08:19 +0200

    creazione menu personale

--- count-ctr.php
+++ count-ctr.php
@@ -7,6 +7,42 @@ Author: Gabriele Pieretti
 Author URI: http://www.stiip.it
 */
 
+/** Add action Ritle pluin page */
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** CSS Style Ritle pluin page  */
+function admin_register_head() {
+    $siteurl = get_option('siteurl');
+    $url = $siteurl . '/wp-content/plugins/' . basename(dirname(__FILE__)) . '/ritleadmin.css';
+    echo "<link rel='stylesheet' type='text/css' href='$url' />\n";
+}
+add_action('admin_head', 'admin_register_head');
+
+/** Function Ritle pluin page  */
+function my_plugin_menu() {
+	add_menu_page( 'My Plugin Options', 'Ritle', 'manage_options', 'my-unique-identifier', 'my_plugin_options', 'dashicons-palmtree');
+}
+
+/** HTML Ritle pluin page only for admin users */
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<h1>Ritle - #1 Ottimizza il Tuo Title</h1>';
+	echo '<p>Inserisci il purchase code che ti è stato fornito in fase di acquisto</p>';
+	echo '<table class="form-table">';
+	echo '<tbody>';
+	echo '<tr>';
+	echo '<th scope="row">';
+	echo '<label for="blogname">Codice API</label>';
+	echo '</th>';
+	echo '<td><input name="blogname" type="text" id="blogname" value="Inserisci il codice di API" class="regular-text"></td>';
+	echo '</tr>';
+	echo '</tbody>';
+	echo '</table>';
+	echo '</div>';
+}
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
@@ -16,7 +52,7 @@ function calcCTR(){
 
 	$title = html_entity_decode( get_the_title(), ENT_QUOTES, 'UTF-8' );
 	if( $title != ''){
-		
+
 
 		$response = json_decode( wp_remote_retrieve_body(wp_remote_post('http://dev.stiip.it/api-ctr/calc-ctr.php', array(
 				    'body'      => ['title' => $title],
